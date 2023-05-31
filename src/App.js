e0801e2bba8053f2f8aae39936861e6316cd044d
Jorge Salgado <j.salgado2k@gmail.com>
2023-05-31 09:26:18 -0400

    Update App.js
    Minor correction con h1 tag

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from V2</h2>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
